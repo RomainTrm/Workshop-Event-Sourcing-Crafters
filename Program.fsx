18355b7784c24ad90d81c395c351b539cbad9817
Romain BERTHON <>
2025-08-22 11:44:19 +0200

    Init project

--- Program.fsx
+++ Program.fsx
@@ -8,8 +8,38 @@
 // Romain Berthon
 //
 // - Blog post : https://berthon.dev/posts/from-a-state-based-to-an-event-sourced-codebase/
+// - Blog post : https://berthon.dev/posts/refining-software-architectures/
+//
 // ---------------------------------------------------------------------
 
-[<EntryPoint>]
-let main argv =
-    0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#r "nuget: FSharp.SystemTextJson,1.4.36"
+
+open System.IO
+open System.Text.Json
+open System.Text.Json.Serialization
+
+let jsonOptions = JsonFSharpOptions.Default().ToJsonSerializerOptions()
+
+let [<Literal>] StateFile = "State"
+let [<Literal>] EventsFile = "Events"
